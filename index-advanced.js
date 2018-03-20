7fc559c939540e5a046dd4006e4aa1e52027cda9
Barton Phillips <bartonphillips@gmail.com>
2018-03-20 03:45:55 -0700

    Changed the way we decide which files should be PUSHED. Added:   let file = fs.readFileSync(path.join(__dirname, 'public', `${urlName}`), {encoding: 'utf8'})              .split('\n')              .filter(line => line.match(/(?:src|href)\s*=\s*[\'\"](.*)[\'\"]/) != null)              .map(line => line.match(/(?:src|href)\s*=\s*[\'\"](.*)[\'\"]/)[1]); To the apt.use() that does the work. Added neo-async and mime to pakage.json.
    modified:   .gitignore
    copied:     public/bundle2.js -> bundle2.js.org
    modified:   index-advanced.js
    new file:   package-lock.json
    modified:   package.json
    new file:   public/bundle.css
    modified:   public/bundle2.js
    new file:   public/bundle3.js
    modified:   public/index.html

--- index-advanced.js
+++ index-advanced.js
@@ -1,78 +1,86 @@
-var express = require('express')
-var app = express()
-const fs = require('fs')
-const path = require('path')
-const url = require('url')
+var express = require('express');
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
+const url = require('url');
 
-let files = {}
-fs.readdir('public', (error, data)=>{
-  data.forEach(name=>{
-    files[`${name}`]=fs
-      .readFileSync(path.join(__dirname, 'public', `${name}`), {encoding: 'utf8'})
-      .split('\n')
-      .filter(line=>line.match(/src *?= *?"(.*)"/)!=null)
-      .map(line=>line.match(/src *?= *?"(.*)"/)[1])
-  })
-  console.log(files)
-})
+var app = express();
 
-const logger = require('morgan')
+const logger = require('morgan');
+
 app.use(logger('dev'))
-app.use((request, response, next)=>{
-  let urlName = url.parse(request.url).pathname.substr(1)
 
-  if (urlName === '' || urlName === '/') urlName = 'index.html'
-  console.log('Request for: ', urlName)
-  if (files[urlName]) {
-    let assets = files[urlName]
-      .filter(name=>(name.substr(0,4)!='http'))
-      .map((fileToPush)=>{
-        let fileToPushPath = path.join(__dirname, 'public', fileToPush)
-        return (cb)=>{
-          if (fileToPushPath.length>100) return cb()
-          fs.readFile(fileToPushPath, (error, data)=>{
-            if (error) return cb(error)
-            console.log('Will push: ', fileToPush, fileToPushPath)
-            try {
-              response.push(`/${fileToPush}`, {}).end(data)
-              cb()
-            } catch(e) {
-              cb(e)
-            }
+app.use((request, response, next) => {
+  let urlName = url.parse(request.url).pathname.substr(1); // remove the /
+  
+  if(urlName === '' || urlName === '/') urlName = 'index.html';
+  console.log('urlName:', urlName);
 
-          })
-        }
-      })
-    // Uncomment to disable server push
-    // assets = []
-    console.log('Total number of assets to push: ', assets.length)
-    assets.unshift((cb)=>{
-      fs.readFile(path.join(__dirname, 'public', urlName), (error, data)=>{
-        if (error) return cb(error)
-        response.write(data)
-        // console.log(data)
-        cb()
-      })
-    })
+  // Make file an arry of items with 'src' or 'href'
 
-    require('neo-async').parallel(assets, (results)=>{
-      response.end()
-    })
+  let file = fs.readFileSync(path.join(__dirname, 'public', `${urlName}`), {encoding: 'utf8'})
+             .split('\n')
+             .filter(line => line.match(/(?:src|href)\s*=\s*[\'\"](.*)[\'\"]/) != null)
+             .map(line => line.match(/(?:src|href)\s*=\s*[\'\"](.*)[\'\"]/)[1]);
+
+  // If there is something in the 'file' array
+      
+  if(file.length != 0) {
+    let assets = file 
+                 .filter(name => (name.substr(0,4)!='http'))
+                 .map((fileToPush) =>
+    {
+      let fileToPushPath = path.join(__dirname, 'public', fileToPush);
+
+      // return an anonomous function
+      
+      return (cb) => {
+        if(fileToPushPath.length > 100) return cb();
+
+        fs.readFile(fileToPushPath, (error, data) => {
+          if(error) return cb(error);
+          console.log('Will push: ', fileToPush, fileToPushPath);
+          try {
+            let m = mime.getType(path.extname(fileToPush));
+            console.log("mime:", m);
+            response.push(`/${fileToPush}`, {headers: {'Content-Type': m}}).end(data);
+            cb();
+          } catch(e) {
+            cb(e);
+          }
+        });
+      };
+    });
+
+    console.log('Total number of assets to push: ', assets.length);
+
+    // Now we put the function for index into the assets array.
+    
+    assets.unshift((cb) => {
+      fs.readFile(path.join(__dirname, 'public', urlName), (error, data) => {
+        if(error) return cb(error);
+        
+        response.write(data);
+        cb();
+      });
+    });
+    
+    require('neo-async').parallel(assets, () => {
+      response.end();
+    });
   } else {
-    return next()
+    next();
   }
-})
-
+});
 
 var options = {
-  key: fs.readFileSync('./server.key'),
-  cert: fs.readFileSync('./server.crt')
-}
+  key: fs.readFileSync('/etc/letsencrypt/live/www.bartonphillips.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/www.bartonphillips.com/fullchain.pem')
+};
 
 require('spdy')
-  .createServer(options, app)
-  .listen(8080, ()=>{
-    console.log(`Server is listening on https://localhost:8080.
-You can open the URL in the browser.`)
-  }
-)
+.createServer(options, app)
+.listen(8080, () => {
+  console.log(`Server is listening on https://localhost:8080.
+You can open the URL in the browser.`);
+});
